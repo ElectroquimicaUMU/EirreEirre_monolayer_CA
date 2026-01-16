8ba16f33692bf08ccb4af961d557d9e674b99f6d
ElectroquimicaUMU <grupoelectroquimicaumureposito@gmail.com>
2026-01-16 19:12:30 +0100

    Set figure DPI for plots in chrono_app.py

--- chrono_app.py
+++ chrono_app.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from chrono_main import simulate_EirreEirre, simulate_EquasiEquasi
 
+dpi = 400
+
 st.set_page_config(layout="wide")
 st.title("📉 Chronoamperometry: EirreEirre vs EquasiEquasi")
 
@@ -37,7 +39,7 @@ res2_reg = custom_regression(res2.time_star, res2.ln_psi, theta_min, theta_max)
 
 # Plot adimensional current Psi
 st.subheader("Ψ(θ) - Adimensional current response")
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(dpi=dpi)
 ax.plot(res1.time_star, res1.psi, label="EirreEirre")
 ax.plot(res2.time_star, res2.psi, "--", label="EquasiEquasi")
 ax.set_xlabel("θ = t / duration")
@@ -47,7 +49,7 @@ st.pyplot(fig)
 
 # Plot surface coverages
 st.subheader("Surface excesses")
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(dpi=dpi)
 ax.plot(res1.time_star, res1.fO, label="fO (EirreEirre)")
 ax.plot(res1.time_star, res1.fR, label="fR (EirreEirre)")
 ax.plot(res1.time_star, res1.fI, label="fI (EirreEirre)")
@@ -61,7 +63,7 @@ st.pyplot(fig)
 
 # Regression analysis of ln|Ψ|
 st.subheader("Regression: ln|Ψ| vs θ")
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(dpi=dpi)
 mask1 = ~np.isnan(res1.ln_psi)
 mask2 = ~np.isnan(res2.ln_psi)
 ax.plot(res1.time_star[mask1], res1.ln_psi[mask1], label="EirreEirre")
