e7826aaf2018a124c249d47160bb0fd1c29ab8b2
ElectroquimicaUMU <grupoelectroquimicaumureposito@gmail.com>
2026-01-18 00:05:41 +0100

    Fix syntax error in dictionary definition

--- chrono_app.py
+++ chrono_app.py
@@ -87,7 +87,9 @@ df = pd.DataFrame([
         "Model": "EirreEirre",
         "Slope": res1_reg["slope"],
         "Intercept": res1_reg["intercept"],
-        "R^2": res1_reg["r"]*res1_reg["r"]
+        "R^2": res1_reg["r"]*res1_reg["r"],
+        "k1_red": res1["k1"],
+        "k2_red": res1["k2"]
     },
     {
         "Model": "EquasiEquasi",
